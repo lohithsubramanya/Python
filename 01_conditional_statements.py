c8e242693cedccd4f74a565bba2822110202d865
Lohith Subramanya <lohith.subramanya@gmail.com>
2017-06-18 16:36:40 +0530

    Add files via upload

---
+++ 01_conditional_statements.py
@@ -0,0 +1,108 @@
+# a = 4 
+# print type(a)
+
+#int, float, str, long, complex, bool 
+
+# a = complex(a,8)
+# print type(a)
+
+
+# a = True #False
+# print  type(a)
+
+
+# Data type conversions 
+
+# a = float(a)
+# print a
+
+
+
+
+# Operators: 
+
+
+#1. Arithmetic :  + - * / % ** // 
+
+# print 2**3
+
+# 2. Assignment : = += -= *= /= %= **= //= 
+
+# a = a+1
+# a +=1 
+
+# a*=3
+# a=a*3
+
+
+# Comparison :  == > < >= <= !=
+
+
+# Logical : and or not 
+
+#bitwise :  & | ^ 
+
+# Membership Operators : in, not in 
+
+# if 's' not in 'hello':
+# 	print 'yes'
+
+
+# identity : is ,is not 
+
+# a = 45 
+# if a is not 5:
+#   	print 'equal'
+
+
+
+# conditional statement/control structure
+
+# a = 2
+# if a > 5 : 
+# 	print a
+# 	print "inside if block"
+# else: 
+# 	print "inside else block"
+
+# print 'outside'
+
+
+
+
+# a = 0 
+# if a > 0 : 
+# 	print "a is positive"
+
+# elif a ==0:
+# 	print "a = 0"
+
+# else : 
+# 	print "a is negative"
+
+
+# a = 4
+# if a%2 == 0:
+# 	print "a is even"
+# else :
+# 	print "a is odd"
+
+
+# Nested if else
+
+
+# a = 0 
+# b = 4
+
+# if a > 0 : 
+# 	print "a is positive"
+
+# elif a ==0:
+# 	print "a = 0"
+# 	if b>5:
+# 		print 'true'
+# 	else: 
+# 		print 'else block'
+
+# else : 
+# 	print "a is negative"
