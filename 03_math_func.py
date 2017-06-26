3041abc60789d75adeaaceb91b6cc1b8c46b63e1
Lohith Subramanya <lohith.subramanya@gmail.com>
2017-06-26 16:46:27 +0530

    Add files via upload

---
+++ 03_math_func.py
@@ -0,0 +1,92 @@
+#for i in range(1,10):
+	
+# 	for j in range(1,i+1):
+# 		print i,
+# 	print 
+
+#NUMBERS:
+
+#int, float, long, complex
+
+# import math
+
+from math import *
+
+# # print dir(math)
+a= 4.786374238484
+# b=6
+
+# print abs(a) #gives absolute value 
+# print fabs(a) #returns float absolute value
+
+# print cmp(a,b) #-1 if a<b ; 0 if a==b ; 1 if a>b
+# print sqrt(a) #gives the square root of a number in float
+# print pow(2,3)#gives you the power; 2 to the power 3
+# print log(a)#gives the natural log value
+# print log10(a) #gives the log base 10 value
+# print exp(a) #gives e to the power a
+# print max(2,3,56,78) #gives you the maximum value from the list of numbers
+# print min(2,3,-59748) #gives the minimum value from the set of numbers
+# print round(a,4) ## rounds off the decimal places to 4 units
+
+#except for max, min, cmp all the other functions are purely for numbers
+
+# print floor(a) #exactly same as floor division
+# print ceil(a) #just opposite to floor
+#trignometric 
+
+# print sin(a) #all the trignometric functions 
+# print degrees(30) #convert radians into degrees
+# print radians(30) #converts degrees into radians
+# print hypot(7,3) #gives you the hypotenuse considering the two parameters as base and perpendicular
+# print pi #constants present inside math 
+# print e
+
+#RANDOM NUMBERS
+
+# from random import *
+
+# a= [34,56,2,3,78,90]
+
+
+# print choice(a) #picks random values from the list of numbers
+
+# print randrange(1,100, 5) #gives random numbers within this range
+
+#randrange gives only integer random numbers; takes only integer parameters
+
+# print uniform(56-91j,100) #takes either float or integer parameters and returns only float value; no steps size allowed
+
+# print random() #gives a float value from 0 to 1. excluding 1
+
+# shuffle(a)
+# print a
+
+# seed(20000)
+# print random() #0.956034271889
+
+# print random()
+
+# seed(20000)
+# a=random()
+
+
+## To seed a number and to retrieve it 
+
+# seed(1)
+# a=random()
+# seed(2)
+# b=random()
+# seed=(3)
+# c=random()
+
+# d=c
+# if d==a:
+# 	print 1
+# elif d==b:
+# 	print 2
+# elif d==c:
+# 	print 3
+
+#a=raw_input("Enter the number")
+
