e30246060f6e7b724cd067a8b27754ee558e9e7a
Lohith Subramanya <lohith.subramanya@gmail.com>
2017-07-31 20:03:08 +0530

    Add files via upload

---
+++ 10_classes.py
@@ -0,0 +1,29 @@
+# class Abc:
+
+# 	def func(self):
+# 		print 'inside Abc class'
+
+
+# class Xyz (Abc):
+# 	'this is the description'
+# 	var=8
+# 	def __init__(self, name):
+# 		self.name=name
+
+# 	def func(self):
+# 		print 'inside Xyz class'
+
+
+# o = Xyz("Mike")
+
+# print hasattr(o,'age')
+# setattr(o,'age',56)
+# print hasattr(o,'age')
+# print getattr(o,'name')
+# delattr(o,'name')
+
+# print Xyz.__name__
+# print Xyz.__doc__
+# print Xyz.__bases__
+
+# print Xyz.__dict__
