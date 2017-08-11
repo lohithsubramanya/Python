68206887ac7193f768a2942f4c6ab0108e0d247b
Lohith Subramanya <lohith.subramanya@gmail.com>
2017-08-11 08:52:35 +0530

    Add files via upload

---
+++ 12_threads.py
@@ -0,0 +1,33 @@
+### Threads - Parallel execution
+
+# import threading
+
+# class myThread(threading.Thread):
+# 	def __init__(self, id, name):
+# 		threading.Thread.__init__(self)
+# 		self.id = id
+# 		self.name = name
+
+# 	def run(self):
+# 		print "The thread executing is :"+self.name   ###  To create multiple threads we need to call the respective functions inside run
+# 		func()    #### 
+
+# def func():
+# 	for i in range(0,100):
+# 		print i
+
+# thread1 = myThread(1,"thread-1")
+# thread2 = myThread(2,"thread-2")
+
+# thread1.start()
+# thread2.start()
+
+
+#### Socket - In order to establish the connection between the client and server.
+
+# Server file to be ran in the background and the client  to be started thereafter
+
+
+
+### Web Crawlers 
+
